bda763218a8f0a3124a06e0fb4b14f4bf0a897c8
Aaron Hu <aaron.hu@pwc.com>
2017-09-05 15:33:45 +0800

    Initial Commit

---
+++ PwCiOSKit/PwCiOSKit.h
@@ -0,0 +1,19 @@
+//
+//  PwCiOSKit.h
+//  PwCiOSKit
+//
+//  Created by ahu039 on 9/5/17.
+//  Copyright © 2017 PwC Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PwCiOSKit.
+FOUNDATION_EXPORT double PwCiOSKitVersionNumber;
+
+//! Project version string for PwCiOSKit.
+FOUNDATION_EXPORT const unsigned char PwCiOSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PwCiOSKit/PublicHeader.h>
+
+
