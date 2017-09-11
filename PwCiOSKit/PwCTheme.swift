183d83704ccb71b43284b19d8587f29fe96c70d9
Aaron Hu <aaron.hu@pwc.com>
2017-09-11 10:40:28 +0800

    update function as public

--- PwCiOSKit/PwCTheme.swift
+++ PwCiOSKit/PwCTheme.swift
@@ -17,10 +17,10 @@ import UIKit
 
 let SelectedThemeKey = "SelectedThemeForUserDefault"
 
-enum PwCTheme: Int {
+public enum PwCTheme: Int {
     case `default`, graphical ,white, dark
     
-    var mainColor: UIColor {
+    public var mainColor: UIColor {
         switch self {
         case .default:
             return UIColor(red: 87.0/255.0, green: 188.0/255.0, blue: 95.0/255.0, alpha: 1.0)
@@ -33,7 +33,7 @@ enum PwCTheme: Int {
         }
     }
     
-    var barStyle: UIBarStyle {
+    public var barStyle: UIBarStyle {
         switch self {
         case .default, .graphical:
             return .default
@@ -43,13 +43,13 @@ enum PwCTheme: Int {
     }
 
     //Set background color for navigation bar
-    func setNavigationBarBkgColor(color: UIColor) {
+    public func setNavigationBarBkgColor(color: UIColor) {
         UINavigationBar.appearance().barTintColor = color
     }
     
     //the text color in status bar will be white, also for the color of navigation bar title
     //if need to use different color for navigation bar title,call setNavigationBarTitleColor
-    func setLightContent()  {
+    public func setLightContent()  {
         UINavigationBar.appearance().barStyle = .black
         
         //Set white color for leftBarItem or rightBarItem
@@ -58,11 +58,11 @@ enum PwCTheme: Int {
     }
     
     //Set color for navigation bar title
-    func setNavigationBarTitleColor(color: UIColor) {
+    public func setNavigationBarTitleColor(color: UIColor) {
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:color]
     }
     
-    func setNavigationBarBkgImg(imageName: String) {
+    public func setNavigationBarBkgImg(imageName: String) {
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().setBackgroundImage(UIImage(named: imageName), for: .default)
     }
@@ -70,7 +70,7 @@ enum PwCTheme: Int {
     //Customize Navigation Bar back indicator
     //If needed, call this to customize mask image: UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backArrowMask")
 
-    func setNavigationItemBackImage(imageName: String) {
+    public func setNavigationItemBackImage(imageName: String) {
         UINavigationBar.appearance().backIndicatorImage = UIImage(named: imageName)
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: imageName)
     }
@@ -78,8 +78,8 @@ enum PwCTheme: Int {
 
 
 
-struct PwCThemeManager {
-    static func currentTheme() -> PwCTheme {
+public struct PwCThemeManager {
+    static public  func currentTheme() -> PwCTheme {
         if let storedTheme = UserDefaults.standard.value(forKey: SelectedThemeKey) {
             return PwCTheme(rawValue: storedTheme as! Int)!
         } else {
@@ -87,7 +87,7 @@ struct PwCThemeManager {
         }
     }
     
-    static func applyTheme(theme: PwCTheme) {
+    static public func applyTheme(theme: PwCTheme) {
         // 1
         UserDefaults.standard.set(theme.rawValue, forKey: SelectedThemeKey)
         UserDefaults.standard.synchronize()
